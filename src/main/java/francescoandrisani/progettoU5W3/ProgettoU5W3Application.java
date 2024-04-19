bd9a2aee4e1323c0035eea909739e75849e5870b
Francesco Andrisani <fra.andrisani01@gmail.com>
2024-04-19 11:20:48 +0100

    Initial commit. Back-end creation of a wen app that handles events.

---
+++ src/main/java/francescoandrisani/progettoU5W3/ProgettoU5W3Application.java
@@ -0,0 +1,13 @@
+package francescoandrisani.progettoU5W3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProgettoU5W3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProgettoU5W3Application.class, args);
+	}
+
+}
---
+++ src/test/java/francescoandrisani/progettoU5W3/ProgettoU5W3ApplicationTests.java
@@ -0,0 +1,13 @@
+package francescoandrisani.progettoU5W3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProgettoU5W3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
